9bb1e8412384afef1dc1038d1eb12a8be8c592ff
lau2004 <1221103162@student.mmu.edu.my>
2025-12-30 09:31:06 +0800

    feat: implement Material Design components and styles for improved UI consistency

---
+++ app/src/main/java/my/mmu/Kaixuanrssnewsreader/ui/setting/SliderPreference.java
@@ -0,0 +1,128 @@
+package my.mmu.Kaixuanrssnewsreader.ui.setting;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.util.AttributeSet;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.preference.Preference;
+import androidx.preference.PreferenceViewHolder;
+
+import com.google.android.material.slider.Slider;
+
+import my.mmu.Kaixuanrssnewsreader.R;
+
+public class SliderPreference extends Preference {
+
+    private int mValue;
+    private int mMin = 0;
+    private int mMax = 100;
+    private int mStep = 1;
+    private Slider mSlider;
+    private TextView mValueTextView;
+
+    public SliderPreference(@NonNull Context context, @Nullable AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+        init(context, attrs);
+    }
+
+    public SliderPreference(@NonNull Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init(context, attrs);
+    }
+
+    public SliderPreference(@NonNull Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+        init(context, attrs);
+    }
+
+    public SliderPreference(@NonNull Context context) {
+        super(context);
+        init(context, null);
+    }
+
+    private void init(Context context, AttributeSet attrs) {
+        setLayoutResource(R.layout.preference_slider);
+        
+        if (attrs != null) {
+            // Check standard namespaces first, then fallback to no namespace for simple cases
+            // Usually min/max are auto-parsed by SeekBarPreference but we are custom.
+            // We use simple attribute reading for simplicity or custom styleable if defined.
+            // For now, let's hardcode parsing or use standard android attrs if possible via TypedArray.
+            
+            // To keep it simple and robust without defining new attrs.xml immediately:
+            // We'll read specific attributes manually if needed, but standard Preference doesn't have min/max/seekBarIncrement.
+            // We can read "min", "max", "seekBarIncrement" from the AttributeSet using the resource namespace.
+            
+            String androidNs = "http://schemas.android.com/apk/res/android";
+            String appNs = "http://schemas.android.com/apk/res-auto";
+
+            mMin = attrs.getAttributeIntValue(appNs, "min", mMin); // Try app namespace first
+            if (mMin == 0) mMin = attrs.getAttributeIntValue(androidNs, "min", 0);
+            
+            mMax = attrs.getAttributeIntValue(androidNs, "max", 100);
+            
+            // seekBarIncrement is not standard android attr for Preference, usually app attr for SeekBarPreference
+            mStep = attrs.getAttributeIntValue(appNs, "seekBarIncrement", 1);
+        }
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull PreferenceViewHolder holder) {
+        super.onBindViewHolder(holder);
+        
+        mSlider = (Slider) holder.findViewById(R.id.slider);
+        mValueTextView = (TextView) holder.findViewById(R.id.slider_value);
+
+        if (mSlider != null) {
+            mSlider.setValueFrom(mMin);
+            mSlider.setValueTo(mMax);
+            mSlider.setStepSize(mStep);
+            mSlider.setValue(mValue);
+            
+            mSlider.addOnChangeListener((slider, value, fromUser) -> {
+                if (fromUser) {
+                    int val = (int) value;
+                    if (val != mValue) {
+                        mValue = val;
+                        updateValueText();
+                        callChangeListener(val);
+                        persistInt(val);
+                    }
+                }
+            });
+        }
+        
+        updateValueText();
+    }
+
+    private void updateValueText() {
+        if (mValueTextView != null) {
+            mValueTextView.setText(String.valueOf(mValue));
+        }
+    }
+
+    @Override
+    protected Object onGetDefaultValue(TypedArray a, int index) {
+        return a.getInt(index, 0);
+    }
+
+    @Override
+    protected void onSetInitialValue(Object defaultValue) {
+        if (defaultValue == null) {
+            defaultValue = 0;
+        }
+        setValue(getPersistedInt((Integer) defaultValue));
+    }
+
+    public void setValue(int value) {
+        mValue = value;
+        if (mSlider != null) {
+            mSlider.setValue(value);
+        }
+        updateValueText();
+    }
+}
